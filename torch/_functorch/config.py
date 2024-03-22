db0599cc48e5e7f6226031e8395a76990df7fcb2
chilli <chilli@meta.com>
2024-03-22 15:00:51 -0700

    Made several changes to min-cut partitioner that allow it to recompute more things
    ghstack-source-id: c6b552b1c24b8f0ee076e4734dc1267bd893625b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121692

--- torch/_functorch/config.py
+++ torch/_functorch/config.py
@@ -32,7 +32,9 @@ static_weight_shapes = True
 cse = True
 
 # Restricts the amount of computation AOTAutograd can do.
-max_dist_from_bw = 3
+max_dist_from_bw = 1000
+
+ban_recompute_materialized_in_backwards = True
 
 # Enable aggressive_recomputation in the min-cut algorithm in partitioners to reduce
 # memory usage with some penalty of performance. It allows more ops to be considered
