fce52a3ba87c04b8a91bf5fadfaeedc080b4e66f
rzou <zou3519@gmail.com>
2024-03-22 13:46:11 -0700

    Add torch.library.def_blackbox
    This is the entrypoint for defining a blackbox (e.g. PyTorch will
    never peek into it) custom op. In this PR, you can specify backend impls
    and the abstract impl for this op.
    
    NB: most of this PR is docstrings, please don't be intimidated by the
    line count.
    
    There are a number of interesting features:
    - we infer the schema from type hints. In a followup I add the ability
      to manually specify a schema.
    - we infer the namespace for the operator from the module. In a
      followup, we'll add the ability to manually specify a namespace.
    - def_blackbox registrations can override each other. This makes them
      more pleasant to work with in environments like colab.
    - we require that the outputs of the blackbox op do not alias any inputs
      or each other. We enforce this via a runtime check.
    
    Test Plan:
    - new tests
    
    ghstack-source-id: 165cd88503aff64b92898691823b2c89bfdf0a39
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122344

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -19,9 +19,11 @@ from functorch import make_fx
 from torch import Tensor
 from torch._custom_op.impl import custom_op, CustomOp, infer_schema
 from torch._utils_internal import get_file_path_2
+from torch.library import def_blackbox
 from torch.testing._internal.common_cuda import TEST_CUDA
-from torch.testing._internal.custom_op_db import custom_op_db
+from torch.testing._internal.custom_op_db import custom_op_db, numpy_nonzero
 from typing import *  # noqa: F403
+import numpy as np
 
 
 class CustomOpTestCaseBase(TestCase):
@@ -331,7 +333,7 @@ class TestCustomOpTesting(CustomOpTestCaseBase):
             args = [sample_input.input] + list(sample_input.args)
             kwargs = sample_input.kwargs
             if op.op in (
-                torch.ops._torch_testing.numpy_nonzero,
+                numpy_nonzero._opoverload,
                 torch.ops._torch_testing.numpy_nms,
             ):
                 ctx = self.assertRaisesRegex(optests.OpCheckError, "failed with")
@@ -677,6 +679,134 @@ class TestCustomOp(CustomOpTestCaseBase):
                 raise NotImplementedError()
 
             infer_schema(foo, mutated_args={"y"})
+
+    def test_blackbox_basic(self):
+        @def_blackbox(mutated_args=())
+        def f(x: Tensor, y: float) -> Tensor:
+            return x + y
+
+        x = torch.randn(3)
+        y = 3.14
+        z = f(x, y)
+        self.assertEqual(z, x + y)
+
+        @f.impl("cpu")
+        def _(x, y):
+            return x - y
+
+        z = f(x, y)
+        self.assertEqual(z, x - y)
+
+        with self.assertRaises(RuntimeError):
+            with torch._subclasses.fake_tensor.FakeTensorMode():
+                x = torch.randn(3)
+                f(x, y)
+
+        @f.impl_abstract
+        def _(x, y):
+            return torch.empty_like(x)
+
+        with torch._subclasses.fake_tensor.FakeTensorMode():
+            x = torch.randn(3)
+            z = f(x, y)
+            self.assertEqual(z.shape, x.shape)
+
+    @skipIfTorchDynamo("recursive dynamo")
+    @unittest.skipIf(IS_WINDOWS, "torch.compile doesn't work on windows")
+    @unittest.skipIf(
+        sys.version_info >= (3, 12), "torch.compile is not supported on python 3.12+"
+    )
+    def test_blackbox_compile(self):
+        @torch.library.def_blackbox(mutated_args=())
+        def custom_linear(x: Tensor, weight: Tensor, bias: Tensor) -> Tensor:
+            return (x @ weight.t()) + bias
+
+        @custom_linear.impl_abstract
+        def _(x, weight, bias):
+            assert x.dim() == 2
+            assert weight.dim() == 2
+            assert bias.dim() == 1
+            assert x.shape[1] == weight.shape[1]
+            assert weight.shape[0] == bias.shape[0]
+            assert x.device == weight.device
+            return x.new_empty(x.size(0), weight.size(0))
+
+        x = torch.randn(2, 2)
+        weight = torch.randn(2, 2)
+        bias = torch.randn(2)
+        out = torch.compile(custom_linear, backend="eager", fullgraph=True)(
+            x, weight, bias
+        )
+        assert torch.allclose(out, torch.nn.functional.linear(x, weight, bias))
+
+    def test_blackbox_replaces(self):
+        @def_blackbox(mutated_args=())
+        def f(x: Tensor) -> Tensor:
+            return x.sin()
+
+        x = torch.randn(3)
+        y = f(x)
+        self.assertEqual(y, x.sin())
+
+        @def_blackbox(mutated_args=())
+        def f(x: Tensor) -> Tensor:
+            return x.cos()
+
+        y = f(x)
+        self.assertEqual(y, x.cos())
+
+    @unittest.skipIf(not TEST_CUDA, "requires CUDA")
+    def test_blackbox_split_device(self):
+        @def_blackbox(mutated_args=(), types="cpu")
+        def f(x: Tensor) -> Tensor:
+            return x.sin()
+
+        @f.impl("cuda")
+        def _(x: Tensor) -> Tensor:
+            return x.cos()
+
+        x = torch.randn(3)
+        y = f(x)
+        self.assertEqual(y, x.sin())
+        x = x.cuda()
+        y = f(x)
+        self.assertEqual(y, x.cos())
+
+    @unittest.skipIf(not TEST_CUDA, "requires CUDA")
+    def test_blackbox_multi_types(self):
+        @def_blackbox(mutated_args=(), types=("cpu", "cuda"))
+        def f(x: Tensor) -> Tensor:
+            return x.sin()
+
+        x = torch.randn(3)
+        y = f(x)
+        self.assertEqual(y, x.sin())
+        x = x.cuda()
+        y = f(x)
+        self.assertEqual(y, x.sin())
+
+    def test_blackbox_disallows_output_aliasing(self):
+        @def_blackbox(mutated_args=())
+        def f(x: Tensor) -> Tensor:
+            return x.view(-1)
+
+        x = torch.randn(3)
+        with self.assertRaisesRegex(RuntimeError, "may not alias"):
+            f(x)
+
+        @def_blackbox(mutated_args=())
+        def f(x: Tensor) -> Tensor:
+            return x
+
+        x = torch.randn(3)
+        with self.assertRaisesRegex(RuntimeError, "may not alias"):
+            f(x)
+
+    def test_blackbox_namespace_inference(self):
+        self.assertExpectedInline(
+            numpy_nonzero._namespace,
+            """mangled2__torch__testing___internal__custom_op_db""",
+        )
 
     def _generate_examples(self, typ):
         if typ is int:
@@ -1452,7 +1582,7 @@ class TestCustomOp(CustomOpTestCaseBase):
     def test_meta_for_data_dependent_shape_operation(self):
         x = torch.randn(10, device="meta")
         with self.assertRaisesRegex(RuntimeError, "data-dependent output shape"):
-            torch.ops._torch_testing.numpy_nonzero(x)
+            numpy_nonzero(x)
 
     def test_basic_make_fx(self):
         # More serious tests are in our CustomOp opinfo db,
@@ -1494,31 +1624,16 @@ class TestCustomOp(CustomOpTestCaseBase):
         with self.assertRaisesRegex(NotImplementedError, "no Tensor inputs"):
             op((1, 2, 3))
 
-    def test_abstract_registration_location(self):
-        custom_op = torch._custom_op.impl._find_custom_op(
-            "_torch_testing::numpy_nonzero"
-        )
-        source = torch._library.simple_registry.singleton.find(
-            "_torch_testing::numpy_nonzero"
-        ).abstract_impl.kernel.source
-        self.assertRegex(source, r".*custom_op_db.py:\d+")
-
     def test_data_dependent_basic(self):
-        def f(x):
-            return torch.ops._torch_testing.numpy_nonzero(x)
-
         x = torch.randn(5, 5)
-        gm = make_fx(f, tracing_mode="symbolic")(x)
+        gm = make_fx(numpy_nonzero, tracing_mode="symbolic")(x)
         self.assertTrue("nonzero" in gm.code)
 
     def test_data_dependent_fake_tracing(self):
-        def f(x):
-            return torch.ops._torch_testing.numpy_nonzero(x)
-
         x = torch.randn(5, 5)
         # We've updated to attempt to use unbacked symints even for fake
         # tracing
-        make_fx(f, tracing_mode="fake")(x)
+        make_fx(numpy_nonzero, tracing_mode="fake")(x)
 
     def test_symints(self):
         def f(x):
@@ -1552,15 +1667,17 @@ def forward(self, x_1):
 
         @torch.compile(backend=cnt)
         def f(x):
-            return torch.ops._torch_testing.numpy_nonzero(x.clone()).clone()
+            return numpy_nonzero(x.clone()).clone()
 
         f(torch.randn(10))
 
-        self.assertEqual(
-            dict(counters["graph_break"]),
-            {
-                "dynamic shape operator: _torch_testing.numpy_nonzero.default; to enable, set torch._dynamo.config.capture_dynamic_output_shape_ops = True": 1  # noqa: B950
-            },
+        self.assertEqual(len(counters["graph_break"]), 1)
+        self.assertEqual(next(iter(counters["graph_break"].values())), 1)
+        self.assertExpectedInline(
+            next(iter(counters["graph_break"].keys())).replace(";", "\n"),
+            """\
+dynamic shape operator: mangled2__torch__testing___internal__custom_op_db.numpy_nonzero.default
+ to enable, set torch._dynamo.config.capture_dynamic_output_shape_ops = True""",
         )
 
     # pre-existing problem: torch.compile(dynamic=True) will, by default,
